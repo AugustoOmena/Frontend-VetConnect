4e6a370e75922a04d9a864674c219c29a8829ab4
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-11-04 11:06:09 -0300

    Create sidebar menu responsive.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'dashboardUser-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], 
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Menu toggle state:', this.isMenuOpen);
+  }
 }
