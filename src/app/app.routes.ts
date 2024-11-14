8c94128023eab0d0377e91ce9b883cc1442536d8
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-11-14 12:05:59 -0300

    Create UsersComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { IsLoggedInGuard } from './core/authentication/auth.guard';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { ServicesComponent } from './features/services/services.component';
 import { PetsComponent } from './features/pets/pets.component';
+import { UsersComponent } from './features/users/users.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: DashboardComponent, canActivate: [IsLoggedInGuard]},
   { path: 'services', component: ServicesComponent, canActivate: [IsLoggedInGuard]},
-  { path: 'pets', component: PetsComponent, canActivate: [IsLoggedInGuard]}
+  { path: 'pets', component: PetsComponent, canActivate: [IsLoggedInGuard]},
+  { path: 'users', component: UsersComponent, canActivate: [IsLoggedInGuard]},
 
 ];
