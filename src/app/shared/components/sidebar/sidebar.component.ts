06db5ceb1408b18cfa0ee909c8935e512d6325ab
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-11-17 15:07:22 -0300

    Dashboard routes update and sidebar perfil.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { EUserType } from '../../Enums/eusertype';
 import { AppService } from '../../../core/services/app.services';
@@ -14,15 +14,26 @@ import { AppService } from '../../../core/services/app.services';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   isMenuOpen = false;
+  perfil!: any;
 
   constructor(
     private appService: AppService
   ) {}
 
+  ngOnInit(): void {
+    const accessUser = JSON.parse(localStorage.getItem('accessUser') || '{}');
+
+    this.perfil = accessUser;
+  }
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  getStatusClass(attendanceStatus: number): string {
+    return EUserType[attendanceStatus]
   }
 
   validateProfile = (profiles: EUserType[]) => this.appService.validateProfile(profiles);
